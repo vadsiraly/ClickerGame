4a56fd76a7df33a4751fed987ccd387bea2c4d76
vadsiraly <nodhtirl@gmail.com>
2018-02-24 17:50:04 +0100

    Added native android project. Created UI draft.

--- ClickerGame/ClickerGame/GeneratorsPage.xaml.cs
+++ ClickerGame/ClickerGame/GeneratorsPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using ClickerEngine;
 using ClickerEngine.Generators;
 using ClickerGame.ViewModel;
+using MWX.XamForms.Popup;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -16,12 +18,51 @@ namespace ClickerGame
     public partial class GeneratorsPage : ContentPage
     {
         private GeneratorsPageViewModel _viewModel;
+        private Popup InfoPopup;
 
         public GeneratorsPage(Engine engine)
         {
             InitializeComponent();
             _viewModel = new GeneratorsPageViewModel(engine);
             BindingContext = _viewModel;
+
+            InfoPopup = new Popup
+            {
+                XPositionRequest = 0.5,
+                YPositionRequest = 0.2,
+                ContentHeightRequest = 0.1,
+                ContentWidthRequest = 0.4,
+                Padding = 10,
+
+                Body = new ContentView
+                {
+                    BackgroundColor = Color.Black,
+                    Content = new Label
+                    {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        VerticalTextAlignment = TextAlignment.Center,
+                        TextColor = Color.White,
+                        Text = "Popup!"
+                    }
+                }
+            };
+
+            new PopupPageInitializer(this) { InfoPopup };
+        }
+
+        private void ShowInfoPopup(object sender, MR.Gestures.LongPressEventArgs e)
+        {
+            InfoPopup.Show();
+        }
+
+        private void HideInfoPopup(object sender, MR.Gestures.LongPressEventArgs e)
+        {
+            InfoPopup.Hide();
+        }
+
+        private void DisableSelection(object sender, EventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
         }
 
         private void Scale_Toggled(object sender, EventArgs e)
---
+++ ClickerGameNative/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using ClickerEngine;
+
+namespace ClickerGame.AndroidNative
+{
+    [Activity(Label = "ClickerGame.AndroidNative", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Engine _engine;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            _engine = new Engine();
+
+            RequestWindowFeature(Android.Views.WindowFeatures.NoTitle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
